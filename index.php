30e8c22e56627b679576f0869066b705452addbb
bpesquet <bpesquet@gmail.com>
2013-09-20 13:49:37 +0200

    Simplifications

--- index.php
+++ index.php
@@ -3,35 +3,18 @@
     <head>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="style.css" />
-        <title>Mon Blog - Sans MVC</title>
+        <title>Mon Blog</title>
     </head>
     <body>
         <div id="global">
             <header>
-                <h1 id="titreBlog"><a href="index.php">Mon Blog</a></h1>
+                <a href="index.php"><h1 id="titreBlog">Mon Blog</h1></a>
                 <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
             </header>
-            <nav>
-                <section>
-                    <h1>Billets</h1>
-                    <ul>
-                        <li><a href="todo">Billets récents</a></li>
-                        <li><a href="todo">Tous les billets</a></li>
-                    </ul>
-                </section>
-                <section>
-                    <h1>Administration</h1>
-                    <ul>
-                        <li><a href="todo">Ecrire un billet</a></li>
-                    </ul>
-                </section>
-            </nav>
             <div id="contenu">
                 <?php
-                // Connexion à la base "monblog" locale, utilisation du compte root sans mot de passe
-                $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'root', '');
-                $requeteBillets = "select * from T_BILLET order by BIL_ID desc";
-                $billets = $bdd->query($requeteBillets);
+                $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $billets = $bdd->query('select * from T_BILLET order by BIL_ID desc');
                 foreach ($billets as $billet):
                     ?>
                     <article>
