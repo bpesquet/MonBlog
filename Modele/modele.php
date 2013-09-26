3d0c493cd28c6f174abdcd237fdecac643c89618
bpesquet <bpesquet@gmail.com>
2013-09-26 10:23:03 +0200

    Renommage PDOStatements

--- Modele/modele.php
+++ Modele/modele.php
@@ -12,12 +12,12 @@ function getBillets() {
 // Renvoie les informations sur un billet
 function getBillet($idBillet) {
     $bdd = getBdd();
-    $stmtBillet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
+    $billet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
             . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
             . ' where BIL_ID=?');
-    $stmtBillet->execute(array($idBillet));
-    if ($stmtBillet->rowCount() > 0)
-        return $stmtBillet->fetch();  // Accès à la première ligne de résultat
+    $billet->execute(array($idBillet));
+    if ($billet->rowCount() > 0)
+        return $billet->fetch();  // Accès à la première ligne de résultat
     else
         throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
 }
@@ -25,11 +25,11 @@ function getBillet($idBillet) {
 // Renvoie la liste des commentaires associés à un billet
 function getCommentaires($idBillet) {
     $bdd = getBdd();
-    $stmtCommentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
+    $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
             . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
             . ' where BIL_ID=?');
-    $stmtCommentaires->execute(array($idBillet));
-    return $stmtCommentaires;
+    $commentaires->execute(array($idBillet));
+    return $commentaires;
 }
 
 // Effectue la connexion à la BDD
