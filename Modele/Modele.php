2aaeb557009b430a2942318d5ebee5ff828331de
bpesquet <bpesquet@gmail.com>
2013-09-26 12:11:52 +0200

    Commentaires

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -13,7 +13,7 @@ abstract class Modele {
     private $bdd;
 
     /**
-     * Exécute une requête SQL
+     * Exécute une requête SQL éventuellement paramétrée
      * 
      * @param string $sql La requête SQL
      * @param array $valeurs Les valeurs associées à la requête
@@ -21,17 +21,17 @@ abstract class Modele {
      */
     protected function executerRequete($sql, $params = null) {
         if ($params == null) {
-            $resultat = $this->getBdd()->query($sql);
+            $resultat = $this->getBdd()->query($sql); // exécution directe
         }
         else {
-            $resultat = $this->getBdd()->prepare($sql);
+            $resultat = $this->getBdd()->prepare($sql);  // requête préparée
             $resultat->execute($params);
         }
         return $resultat;
     }
 
     /**
-     * Renvoie un objet de connexion à la BDD en initialisant la connexion au besoin
+     * Renvoie un objet de connexion à la BD en initialisant la connexion au besoin
      * 
      * @return PDO L'objet PDO de connexion à la BDD
      */
