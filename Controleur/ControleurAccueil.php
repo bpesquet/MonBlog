1405ce8d8bc2ec84ce07198b6472f84f6d2b825b
bpesquet <bpesquet@gmail.com>
2013-09-26 17:15:18 +0200

    Suppression de lienBillet dans le contrôleur

--- Controleur/ControleurAccueil.php
+++ Controleur/ControleurAccueil.php
@@ -14,9 +14,8 @@ class ControleurAccueil {
 // Affiche la liste de tous les billets du blog
     public function accueil() {
         $billets = $this->billet->getBillets();
-        $lienBillet = "index.php?action=billet&id=";
         $vue = new Vue("Accueil");
-        $vue->generer(array('billets' => $billets, 'lienBillet' => $lienBillet));
+        $vue->generer(array('billets' => $billets));
     }
 
 }
