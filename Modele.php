3fce9029e4b1a9058ff260c277a142eda2ba72e7
bpesquet <bpesquet@gmail.com>
2013-09-26 11:03:14 +0200

    Ajout fichier Modèle

---
+++ Modele.php
@@ -0,0 +1,41 @@
+<?php
+
+// Renvoie la liste des billets du blog
+function getBillets() {
+    $bdd = getBdd();
+    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+            . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+            . ' order by BIL_ID desc');
+    return $billets;
+}
+
+// Renvoie les informations sur un billet
+function getBillet($idBillet) {
+    $bdd = getBdd();
+    $billet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
+            . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+            . ' where BIL_ID=?');
+    $billet->execute(array($idBillet));
+    if ($billet->rowCount() > 0)
+        return $billet->fetch();  // Accès à la première ligne de résultat
+    else
+        throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
+}
+
+// Renvoie la liste des commentaires associés à un billet
+function getCommentaires($idBillet) {
+    $bdd = getBdd();
+    $commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
+            . ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
+            . ' where BIL_ID=?');
+    $commentaires->execute(array($idBillet));
+    return $commentaires;
+}
+
+// Effectue la connexion à la BDD
+// Instancie et renvoie l'objet PDO associé
+function getBdd() {
+    $bdd = new PDO('mysql:host=localhost;dbname=monblog;charset=utf8', 'root',
+            '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return $bdd;
+}
