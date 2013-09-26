e27f8b77aa142c3e3bebb456d47d5b3d384a38a6
bpesquet <bpesquet@gmail.com>
2013-09-26 11:50:11 +0200

    Renommage PDOStatements

--- Modele/Billet.php
+++ Modele/Billet.php
@@ -31,9 +31,9 @@ class Billet extends Modele {
         $sql = 'select BIL_ID as id, BIL_DATE as date,'
                 . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
                 . ' where BIL_ID=?';
-        $stmtBillet = $this->executerRequete($sql, array($idBillet));
-        if ($stmtBillet->rowCount() > 0)
-            return $stmtBillet->fetch();  // Accès à la première ligne de résultat
+        $billet = $this->executerRequete($sql, array($idBillet));
+        if ($billet->rowCount() > 0)
+            return $billet->fetch();  // Accès à la première ligne de résultat
         else
             throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
     }
