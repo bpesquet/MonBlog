6ced10c35ee4733c5a581f54a4ea9f951423a766
Baptiste Pesquet <bpesquet@gmail.com>
2013-10-08 17:46:24 +0200

    Inversion du fichier de conf chargé en premier

--- Framework/Configuration.php
+++ Framework/Configuration.php
@@ -33,16 +33,16 @@ class Configuration {
 
     /**
      * Renvoie le tableau des paramètres en le chargeant au besoin depuis un fichier de configuration.
-     * Les fichiers de configuration recherchés sont Config/prod.ini et Config/dev.ini (dans cet ordre)
+     * Les fichiers de configuration recherchés sont Config/dev.ini et Config/prod.ini (dans cet ordre)
      * 
      * @return array Tableau des paramètres
      * @throws Exception Si aucun fichier de configuration n'est trouvé
      */
     private static function getParametres() {
         if (self::$parametres == null) {
-            $cheminFichier = "Config/prod.ini";
+            $cheminFichier = "Config/dev.ini";
             if (!file_exists($cheminFichier)) {
-                $cheminFichier = "Config/dev.ini";
+                $cheminFichier = "Config/prod.ini";
             }
             if (!file_exists($cheminFichier)) {
                 throw new Exception("Aucun fichier de configuration trouvé");
