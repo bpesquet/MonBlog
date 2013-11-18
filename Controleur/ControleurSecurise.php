aef7f07d518c7e28b8c8013f04d772b9d0edd87f
bpesquet <bpesquet@gmail.com>
2013-11-18 18:28:35 +0100

    Orthographe

--- Controleur/ControleurSecurise.php
+++ Controleur/ControleurSecurise.php
@@ -12,7 +12,7 @@ abstract class ControleurSecurise extends Controleur
 
     public function executerAction($action)
     {
-        // Vérifie si les informations utilisateurs sont présents dans la session
+        // Vérifie si les informations utilisateur sont présents dans la session
         // Si oui, l'utilisateur s'est déjà authentifié : l'exécution de l'action continue normalement
         // Si non, l'utilisateur est renvoyé vers le contrôleur de connexion
         if ($this->requete->getSession()->existeAttribut("idUtilisateur")) {
