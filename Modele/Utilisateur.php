3815873e5ce3767f77fcc704ddfa826f67246ad6
bpesquet <bpesquet@gmail.com>
2013-11-22 18:23:11 +0100

    Commentaires

--- Modele/Utilisateur.php
+++ Modele/Utilisateur.php
@@ -9,6 +9,13 @@ require_once 'Framework/Modele.php';
  */
 class Utilisateur extends Modele {
 
+    /**
+     * Vérifie qu'un utilisateur existe dans la BD
+     * 
+     * @param string $login Le login
+     * @param string $mdp Le mot de passe
+     * @return boolean Vrai si l'utilisateur existe, faux sinon
+     */
     public function connecter($login, $mdp)
     {
         $sql = "select UTIL_ID from T_UTILISATEUR where UTIL_LOGIN=? and UTIL_MDP=?";
@@ -16,6 +23,14 @@ class Utilisateur extends Modele {
         return ($utilisateur->rowCount() == 1);
     }
 
+    /**
+     * Renvoie un utilisateur existant dans la BD
+     * 
+     * @param string $login Le login
+     * @param string $mdp Le mot de passe
+     * @return mixed L'utilisateur
+     * @throws Exception Si aucun utilisateur ne correspond aux paramètres
+     */
     public function getUtilisateur($login, $mdp)
     {
         $sql = "select UTIL_ID as idUtilisateur, UTIL_LOGIN as login, UTIL_MDP as mdp 
